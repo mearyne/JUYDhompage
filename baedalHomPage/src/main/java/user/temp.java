eb18efe1e10baac07721563d2833f66ce2bd21af
woo-dayoung <daisy123659@gmail.com>
2022-07-11 11:37:57 +0900

    db연동 test

---
+++ baedalHomPage/src/main/java/user/temp.java
@@ -0,0 +1,12 @@
+package user;
+
+
+public class temp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		userDAO dao=userDAO.getInstance();
+		dao.addUser();
+	}
+
+} 
