ecc20988d368456e4f1694a90cad2236240b6fd8
Song-yoon-joo <qlqj0101@gmail.com>
2022-07-11 11:05:51 +0900

    송윤주 DTO

--- baedalHomPage/src/main/java/cart/cartDTO.java
+++ baedalHomPage/src/main/java/cart/cartDTO.java
@@ -1,5 +1,49 @@
 package cart;
 
 public class cartDTO {
-	// cartDTO
+	private int cardCode;
+	private int userCode;
+	private int shopCode;
+	private int menuCode;
+	private int cartNum;
+	
+	
+	public cartDTO(int cardCode, int userCode, int shopCode, int menuCode, int cartNum) {
+		super();
+		this.cardCode = cardCode;
+		this.userCode = userCode;
+		this.shopCode = shopCode;
+		this.menuCode = menuCode;
+		this.cartNum = cartNum;
+	}
+	public int getCardCode() {
+		return cardCode;
+	}
+	public void setCardCode(int cardCode) {
+		this.cardCode = cardCode;
+	}
+	public int getUserCode() {
+		return userCode;
+	}
+	public void setUserCode(int userCode) {
+		this.userCode = userCode;
+	}
+	public int getShopCode() {
+		return shopCode;
+	}
+	public void setShopCode(int shopCode) {
+		this.shopCode = shopCode;
+	}
+	public int getMenuCode() {
+		return menuCode;
+	}
+	public void setMenuCode(int menuCode) {
+		this.menuCode = menuCode;
+	}
+	public int getCartNum() {
+		return cartNum;
+	}
+	public void setCartNum(int cartNum) {
+		this.cartNum = cartNum;
+	}
 }
--- baedalHomPage/src/main/java/order/orderDTO.java
+++ baedalHomPage/src/main/java/order/orderDTO.java
@@ -1,5 +1,53 @@
 package order;
 
+import java.security.Timestamp;
+
+
+
 public class orderDTO {
-	// orderDTO
+	private int oderCode;
+	private int userCode;
+	private int shopCode;
+	private int menuCode;
+	private Timestamp bookingDate;
+	
+
+	public orderDTO(int oderCode, int userCode, int shopCode, int menuCode, Timestamp bookingDate) {
+		super();
+		this.oderCode = oderCode;
+		this.userCode = userCode;
+		this.shopCode = shopCode;
+		this.menuCode = menuCode;
+		this.bookingDate = bookingDate;
+	}
+	public int getOderCode() {
+		return oderCode;
+	}
+	public void setOderCode(int oderCode) {
+		this.oderCode = oderCode;
+	}
+	public int getUserCode() {
+		return userCode;
+	}
+	public void setUserCode(int userCode) {
+		this.userCode = userCode;
+	}
+	public int getShopCode() {
+		return shopCode;
+	}
+	public void setShopCode(int shopCode) {
+		this.shopCode = shopCode;
+	}
+	public int getMenuCode() {
+		return menuCode;
+	}
+	public void setMenuCode(int menuCode) {
+		this.menuCode = menuCode;
+	}
+	public Timestamp getBookingDate() {
+		return bookingDate;
+	}
+	public void setBookingDate(Timestamp bookingDate) {
+		this.bookingDate = bookingDate;
+	}
 }
